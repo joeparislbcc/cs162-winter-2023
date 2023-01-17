0a2e7f932467c9f1d165d3e099ea90f8e4c13227
Joe Paris <2662123+joeparis@users.noreply.github.com>
2023-01-17 08:37:20 -0800

    Removed early access to demo code

--- demos/23.01.18/program11-8.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2017, 2013, 2011 Pearson Education, Inc., W.F. Punch & R.J.Enbody
-
-class NewClass (object):
-    def __init__(self, attribute='default', name='Instance'):
-        self.name = name               # public attribute
-        self.__attribute = attribute   # a "private" attribute
-    def __str__(self):
-        return '{} has attribute {}'.format(self.name, self.__attribute)
